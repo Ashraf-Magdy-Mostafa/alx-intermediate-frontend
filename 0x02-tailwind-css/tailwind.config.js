0728d323f7ab4f9244406696628e88bf6e6f3d20
Ashraf Magdy Mostafa <ashrafmagdy926@gmail.com>
2025-07-05 04:19:06 +0300

    tweaked input.css and populated 1-index.html

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,8 +1,9 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: ["./index.html", "./src/**/*.{html,js}"],
     theme: {
         extend: {},
     },
     plugins: [],
-}
+};
